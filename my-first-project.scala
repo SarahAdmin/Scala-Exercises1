47b0c60780857a1e0daadc67da2d22d1c6587cdc
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-10-09 16:10:15 +0100

    Update my-first-project.scala

--- my-first-project.scala
+++ my-first-project.scala
@@ -1,6 +1,10 @@
 import Example.* 
 
-@main def hello: Unit = ()
+@main def hello: Unit = 
+println("Hello world!")
+println(message)
+
+def message = "This is my first application"
 
 object Example { 
 
